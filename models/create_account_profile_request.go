000783471484bd2647310650765ef07bd5a62b60
Chance Zibolski <chance@coreos.com>
2016-02-12 15:06:52 -0800

    models: No required fields on CreateAccountProfileRequest

--- models/create_account_profile_request.go
+++ models/create_account_profile_request.go
@@ -5,7 +5,6 @@ package models
 
 import (
 	"github.com/go-swagger/go-swagger/errors"
-	"github.com/go-swagger/go-swagger/httpkit/validate"
 	"github.com/go-swagger/go-swagger/strfmt"
 	"github.com/go-swagger/go-swagger/swag"
 )
@@ -17,10 +16,8 @@ swagger:model CreateAccountProfileRequest
 type CreateAccountProfileRequest struct {
 
 	/* { "description" : "", "verbs":["GET"] }
-
-	Required: true
-	*/
-	AccountID string `json:"accountID,omitempty"`
+	 */
+	AccountID *string `json:"accountID,omitempty"`
 
 	/* { "description" : "Any additional information", "verbs":["POST","PUT","GET"] }
 	 */
@@ -39,30 +36,24 @@ type CreateAccountProfileRequest struct {
 	Dob strfmt.DateTime `json:"dob,omitempty"`
 
 	/* { "description" : "E-mail address", "verbs":["POST","PUT","GET"] }
-
-	Required: true
-	*/
-	Email string `json:"email,omitempty"`
+	 */
+	Email *string `json:"email,omitempty"`
 
 	/* { "description" : "Fax number", "verbs":["POST","PUT","GET"] }
 	 */
 	Fax *string `json:"fax,omitempty"`
 
 	/* { "description" : "", "verbs":["POST","PUT","GET"] }
-
-	Required: true
-	*/
-	FirstName string `json:"firstName,omitempty"`
+	 */
+	FirstName *string `json:"firstName,omitempty"`
 
 	/* { "description" : "Home telephone number", "verbs":["POST","PUT","GET"] }
 	 */
 	Landline *string `json:"landline,omitempty"`
 
 	/* { "description" : "", "verbs":["POST","PUT","GET"] }
-
-	Required: true
-	*/
-	LastName string `json:"lastName,omitempty"`
+	 */
+	LastName *string `json:"lastName,omitempty"`
 
 	/* { "description" : "", "verbs":["POST","PUT","GET"] }
 	 */
@@ -81,27 +72,7 @@ type CreateAccountProfileRequest struct {
 func (m *CreateAccountProfileRequest) Validate(formats strfmt.Registry) error {
 	var res []error
 
-	if err := m.validateAccountID(formats); err != nil {
-		// prop
-		res = append(res, err)
-	}
-
 	if err := m.validateAddresses(formats); err != nil {
-		// prop
-		res = append(res, err)
-	}
-
-	if err := m.validateEmail(formats); err != nil {
-		// prop
-		res = append(res, err)
-	}
-
-	if err := m.validateFirstName(formats); err != nil {
-		// prop
-		res = append(res, err)
-	}
-
-	if err := m.validateLastName(formats); err != nil {
 		// prop
 		res = append(res, err)
 	}
@@ -109,15 +80,6 @@ func (m *CreateAccountProfileRequest) Validate(formats strfmt.Registry) error {
 	if len(res) > 0 {
 		return errors.CompositeValidationError(res...)
 	}
-	return nil
-}
-
-func (m *CreateAccountProfileRequest) validateAccountID(formats strfmt.Registry) error {
-
-	if err := validate.RequiredString("accountID", "body", string(m.AccountID)); err != nil {
-		return err
-	}
-
 	return nil
 }
 
@@ -136,33 +98,6 @@ func (m *CreateAccountProfileRequest) validateAddresses(formats strfmt.Registry)
 			}
 		}
 
-	}
-
-	return nil
-}
-
-func (m *CreateAccountProfileRequest) validateEmail(formats strfmt.Registry) error {
-
-	if err := validate.RequiredString("email", "body", string(m.Email)); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func (m *CreateAccountProfileRequest) validateFirstName(formats strfmt.Registry) error {
-
-	if err := validate.RequiredString("firstName", "body", string(m.FirstName)); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func (m *CreateAccountProfileRequest) validateLastName(formats strfmt.Registry) error {
-
-	if err := validate.RequiredString("lastName", "body", string(m.LastName)); err != nil {
-		return err
 	}
 
 	return nil
